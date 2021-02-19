d7804338bbd873d5a649cacb637d3516dbcbd826
RARJEF <larinfirelight@gmail.com>
2021-02-19 01:03:16 -0800

    Initial Project Setup
    I'm getting a null point exception, and I'm not sure why.
    Error Message from Logcat:
    2021/02/19 00:58:04.484 3905 20341 Error Unity NullReferenceException: Object reference not set to an instance of an object
    2021/02/19 00:58:04.484 3905 20341 Error Unity   at PlaneAreaBehaviour.Update () [0x00020] in <27be29fc22424df4a4fb11c869d75a7b>:0

---
+++ Assets/Scripts/PlaneAreaBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.XR.ARFoundation;
+
+public class PlaneAreaBehaviour : MonoBehaviour
+{
+    public TextMeshPro areaText;
+    public ARPlane arPlane;
+
+
+    private void OnEnable()
+    {
+        arPlane.boundaryChanged += ArPlane_BoundaryChanged;
+    }
+
+    private void OnDisable()
+    {
+        arPlane.boundaryChanged -= ArPlane_BoundaryChanged;
+    }
+
+    void ArPlane_BoundaryChanged(ARPlaneBoundaryChangedEventArgs obj)
+    {
+        areaText.text = CalculatePlaneArea(arPlane).ToString();
+    }
+
+    private float CalculatePlaneArea(ARPlane plane)
+    {
+        return plane.size.x * plane.size.y;
+    }
+
+    public void ToggleAreaView()
+    {
+        if (areaText.enabled)
+            areaText.enabled = false;
+        else
+            areaText.enabled = true;
+    }
+
+    private void Update()
+    {
+        areaText.transform.rotation = Quaternion.LookRotation(areaText.transform.position - Camera.main.transform.position);
+    }
+}
